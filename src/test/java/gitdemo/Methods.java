6c156d6a93cf67be2cc24d108924f517c2c51f8f
saravananpriyan <saravanapriyanrk@gmail.com>
2021-04-09 18:48:12 +0530

    my task

---
+++ src/test/java/gitdemo/Methods.java
@@ -0,0 +1,17 @@
+package gitdemo;
+
+public class Methods {
+
+	private void method1() {
+	System.out.println("method id is 1");
+}
+	private void method2() {
+		System.out.println("method id is 2");
+	}
+	private void method3() {
+		System.out.println("method id is 3");
+	}
+	private void method4() {
+		System.out.println("method id is 4");
+	}
+}
